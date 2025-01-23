deec470f5e416ebdfef3bc052adefad365cf5889
Col0ring <1561999073@qq.com>
2025-01-23 18:52:26 +0800

    fix: collapse

--- frontend/antd/collapse/collapse.tsx
+++ frontend/antd/collapse/collapse.tsx
@@ -27,13 +27,12 @@ export const Collapse = sveltify<
       ...props
     }) => {
       const { items: slotItems } = useItems<['default', 'items']>();
-      const resolvedSlotItems = slotItems.items
-        ? slotItems.items
-        : slotItems.default;
+      const resolvedSlotItems =
+        slotItems.items.length > 0 ? slotItems.items : slotItems.default;
       const expandIconFunction = useFunction(expandIcon);
       return (
         <>
-          {children}
+          <div style={{ display: 'none' }}>{children}</div>
           <ACollapse
             {...props}
             onChange={(key) => {
