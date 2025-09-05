0d2d8be49d99c96b1c6ef1171c1948c065ce2a3e
Col0ring <1561999073@qq.com>
2025-09-05 16:52:38 +0800

    fix: bugfix in dev mode

---
+++ .changeset/twenty-moles-slide.md
@@ -0,0 +1,5 @@
+---
+'modelscope_studio': patch
+---
+
+fix: bugfix in dev mode
---
+++ backend/modelscope_studio/components/__init__.py
@@ -0,0 +1,5 @@
+from .antd.components import *
+from .antdx.components import *
+from .base import *
+from .legacy.components import *
+from .pro.components import *
